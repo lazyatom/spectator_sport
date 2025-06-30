16e3f94d21a943a6f6cce656d928895a656ec7a7
James Adam <james@lazyatom.com>
2025-06-30 22:55:41 +0100

    Vibe-coded storage of UTM params
    This isn't quite what I wanted; I wanted to be able to supply arbitrary
    metadata from the consumer app and have this engine support it. But I
    want to just get something working now.

--- app/helpers/spectator_sport/script_helper.rb
+++ app/helpers/spectator_sport/script_helper.rb
@@ -1,7 +1,15 @@
 module SpectatorSport
   module ScriptHelper
-    def spectator_sport_script_tags
-      tag.script defer: true, src: spectator_sport.events_path(format: :js)
+    def spectator_sport_script_tags(metadata: {})
+      metadata_script = if metadata.present?
+        tag.script("window.SPECTATOR_SPORT_METADATA = #{metadata.to_json};".html_safe)
+      else
+        tag.script("window.SPECTATOR_SPORT_METADATA = {};".html_safe)
+      end
+
+      main_script = tag.script defer: true, src: spectator_sport.events_path(format: :js)
+
+      metadata_script + main_script
     end
   end
 end
