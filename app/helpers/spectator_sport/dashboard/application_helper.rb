39db00fc1c8e4a7c8669fb6071ce2e44b9bb8b58
James Adam <james@lazyatom.com>
2025-06-30 14:37:40 +0100

    Add simple duration to the session window
    This helps us quickly see which sessions might have more information in
    them than others.

--- app/helpers/spectator_sport/dashboard/application_helper.rb
+++ app/helpers/spectator_sport/dashboard/application_helper.rb
@@ -2,6 +2,23 @@ module SpectatorSport
   module Dashboard
     module ApplicationHelper
       include Dashboard::IconsHelper
+
+      def formatted_duration(duration)
+        seconds = duration.to_i
+        return "0s" if seconds == 0
+
+        hours = seconds / 3600
+        minutes = (seconds % 3600) / 60
+        remaining_seconds = seconds % 60
+
+        if hours > 0
+          "#{hours}h #{minutes}m #{remaining_seconds}s"
+        elsif minutes > 0
+          "#{minutes}m #{remaining_seconds}s"
+        else
+          "#{remaining_seconds}s"
+        end
+      end
     end
   end
 end
--- app/models/spectator_sport/session_window.rb
+++ app/models/spectator_sport/session_window.rb
@@ -2,5 +2,19 @@ module SpectatorSport
   class SessionWindow < ApplicationRecord
     belongs_to :session
     has_many :events
+
+    def duration
+      return 0 if events.empty?
+
+      earliest_event = events.minimum(:created_at)
+      latest_event = events.maximum(:created_at)
+
+      return 0 unless earliest_event && latest_event
+
+      latest_event - earliest_event
+    end
+
+      end
+    end
   end
 end
