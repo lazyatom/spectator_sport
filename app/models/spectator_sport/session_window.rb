2bba795c0c7bb55e8b5faf3f076d54ab35b66779
James Adam <james@lazyatom.com>
2025-06-30 15:03:29 +0100

    Add listing of visited pageship
    This helps us see where each session went

--- app/models/spectator_sport/session_window.rb
+++ app/models/spectator_sport/session_window.rb
@@ -47,5 +47,58 @@ module SpectatorSport
 
       total_active_duration
     end
+
+    def visited_paths
+      return [] if events.empty?
+
+      paths = []
+
+      # Get Meta events (type 4) that contain URL information
+      meta_events = events.where("event_data->>'type' = '4'")
+
+      meta_events.find_each do |event|
+        href = event.event_data.dig('data', 'href')
+        if href.present?
+          begin
+            uri = URI.parse(href)
+            path = uri.path
+            path = '/' if path.blank?
+            paths << path unless paths.include?(path)
+          rescue URI::InvalidURIError
+            # Skip invalid URLs
+          end
+        end
+      end
+
+      # Get Custom events (type 5) for Turbo navigation tracking
+      custom_events = events.where("event_data->>'type' = '5' AND event_data->'data'->>'tag' IN ('navigation', 'page_load')")
+
+      custom_events.find_each do |event|
+        url = event.event_data.dig('data', 'payload', 'url')
+        if url.present?
+          begin
+            uri = URI.parse(url)
+            path = uri.path
+            path = '/' if path.blank?
+            paths << path unless paths.include?(path)
+          rescue URI::InvalidURIError
+            # Skip invalid URLs
+          end
+        end
+      end
+
+      paths
+    end
+
+    def visited_paths_display
+      paths = visited_paths
+      return "No paths" if paths.empty?
+
+      if paths.length <= 3
+        paths.join(", ")
+      else
+        "#{paths.first(3).join(", ")} +#{paths.length - 3} more"
+      end
+    end
   end
 end
