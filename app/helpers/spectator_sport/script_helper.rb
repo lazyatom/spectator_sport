8991d3f350c38809569d7c74a2e5474166b40527
James Adam <james@lazyatom.com>
2025-07-02 11:23:33 +0100

    Allow us to controller when new sessions start
    This means we can retain the same session as people move between our
    subdomains.

--- app/helpers/spectator_sport/script_helper.rb
+++ app/helpers/spectator_sport/script_helper.rb
@@ -1,12 +1,20 @@
 module SpectatorSport
   module ScriptHelper
-    def spectator_sport_script_tags(metadata: {})
-      metadata_script = if metadata.present?
-        tag.script("window.SPECTATOR_SPORT_METADATA = #{metadata.to_json};".html_safe)
-      else
-        tag.script("window.SPECTATOR_SPORT_METADATA = {};".html_safe)
+    def spectator_sport_script_tags(metadata: {}, session_id: nil)
+      # Prepare the configuration object
+      config = {
+        metadata: metadata.presence || {}
+      }
+
+      # Build the initialization script
+      init_script = "window.SPECTATOR_SPORT_CONFIG = #{config.to_json};"
+
+      # Set localStorage session_id if provided
+      if session_id.present?
+        init_script += "window.localStorage.setItem('spectator_sport_session_id', #{session_id.to_json});"
       end
 
+      metadata_script = tag.script(init_script.html_safe)
       main_script = tag.script defer: true, src: spectator_sport.events_path(format: :js)
 
       metadata_script + main_script
